004ede991758f5bdaf86f496792fecddd81a28d7
fozeryPC <fozery@yandex.ru>
2024-02-21 17:18:05 +0300

    fix: fix time keyboard
    Добавил endTime = null, чтобы, если возвращаемся
    в предыдущую сцену и жмём кнопку,
    то endtime сбрасывался

--- utils/keyboards.js
+++ utils/keyboards.js
@@ -3,110 +3,110 @@ const { Keyboard } = require('vk-io');
 const timeKeyboard = Keyboard.builder()
   .textButton({
     label: '00:00',
-    payload: { date: { time: '00:00', textTime: '00:00' } },
+    payload: { date: { time: '00:00', startTime: '00:00', endTime: null } },
   })
   .textButton({
     label: '01:00',
-    payload: { date: { time: '01:00', textTime: '01:00' } },
+    payload: { date: { time: '01:00', startTime: '01:00' } },
   })
   .textButton({
     label: '02:00',
-    payload: { date: { time: '02:00', textTime: '02:00' } },
+    payload: { date: { time: '02:00', startTime: '02:00', endTime: null } },
   })
   .textButton({
     label: '03:00',
-    payload: { date: { time: '03:00', textTime: '03:00' } },
+    payload: { date: { time: '03:00', startTime: '03:00', endTime: null } },
   })
   .textButton({
     label: '04:00',
-    payload: { date: { time: '04:00', textTime: '04:00' } },
+    payload: { date: { time: '04:00', startTime: '04:00', endTime: null } },
   })
 
   .row()
 
   .textButton({
     label: '05:00',
-    payload: { date: { time: '05:00', textTime: '05:00' } },
+    payload: { date: { time: '05:00', startTime: '05:00', endTime: null } },
   })
   .textButton({
     label: '06:00',
-    payload: { date: { time: '06:00', textTime: '06:00' } },
+    payload: { date: { time: '06:00', startTime: '06:00', endTime: null } },
   })
   .textButton({
     label: '07:00',
-    payload: { date: { time: '07:00', textTime: '07:00' } },
+    payload: { date: { time: '07:00', startTime: '07:00', endTime: null } },
   })
   .textButton({
     label: '08:00',
-    payload: { date: { time: '08:00', textTime: '08:00' } },
+    payload: { date: { time: '08:00', startTime: '08:00', endTime: null } },
   })
   .textButton({
     label: '09:00',
-    payload: { date: { time: '09:00', textTime: '09:00' } },
+    payload: { date: { time: '09:00', startTime: '09:00', endTime: null } },
   })
 
   .row()
 
   .textButton({
     label: '10:00',
-    payload: { date: { time: '10:00', textTime: '10:00' } },
+    payload: { date: { time: '10:00', startTime: '10:00', endTime: null } },
   })
   .textButton({
     label: '11:00',
-    payload: { date: { time: '11:00', textTime: '11:00' } },
+    payload: { date: { time: '11:00', startTime: '11:00', endTime: null } },
   })
   .textButton({
     label: '12:00',
-    payload: { date: { time: '12:00', textTime: '12:00' } },
+    payload: { date: { time: '12:00', startTime: '12:00', endTime: null } },
   })
   .textButton({
     label: '13:00',
-    payload: { date: { time: '13:00', textTime: '13:00' } },
+    payload: { date: { time: '13:00', startTime: '13:00', endTime: null } },
   })
   .textButton({
     label: '14:00',
-    payload: { date: { time: '14:00', textTime: '14:00' } },
+    payload: { date: { time: '14:00', startTime: '14:00', endTime: null } },
   })
 
   .row()
 
   .textButton({
     label: '15:00',
-    payload: { date: { time: '15:00', textTime: '15:00' } },
+    payload: { date: { time: '15:00', startTime: '15:00', endTime: null } },
   })
   .textButton({
     label: '16:00',
-    payload: { date: { time: '16:00', textTime: '16:00' } },
+    payload: { date: { time: '16:00', startTime: '16:00', endTime: null } },
   })
   .textButton({
     label: '17:00',
-    payload: { date: { time: '17:00', textTime: '17:00' } },
+    payload: { date: { time: '17:00', startTime: '17:00', endTime: null } },
   })
   .textButton({
     label: '18:00',
-    payload: { date: { time: '18:00', textTime: '18:00' } },
+    payload: { date: { time: '18:00', startTime: '18:00', endTime: null } },
   })
   .textButton({
     label: '19:00',
-    payload: { date: { time: '19:00', textTime: '19:00' } },
+    payload: { date: { time: '19:00', startTime: '19:00', endTime: null } },
   })
 
   .row()
   .textButton({
     label: '20:00',
-    payload: { date: { time: '20:00', textTime: '20:00' } },
+    payload: { date: { time: '20:00', startTime: '20:00', endTime: null } },
   })
   .textButton({
     label: '21:00',
-    payload: { date: { time: '21:00', textTime: '21:00' } },
+    payload: { date: { time: '21:00', startTime: '21:00', endTime: null } },
   })
   .textButton({
     label: '22:00',
-    payload: { date: { time: '22:00', textTime: '22:00' } },
+    payload: { date: { time: '22:00', startTime: '22:00', endTime: null } },
   })
   .textButton({
     label: '23:00',
-    payload: { date: { time: '23:00', textTime: '23:00' } },
+    payload: { date: { time: '23:00', startTime: '23:00', endTime: null } },
   })
   .row()
   .textButton({
