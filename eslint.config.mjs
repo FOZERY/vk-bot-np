0951d20fda38830a3dcbcec4c45cecc54508285c
fozery <fozery@yandex.ru>
2024-05-29 19:15:13 +0300

    fix: fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,21 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import prettierPlugin from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import nodePlugin from 'eslint-plugin-n';
 
-export default [{
-    plugins: {
-        prettier: prettierPlugin,
+
+export default [
+    nodePlugin.configs['flat/recommended-script'],
+    {
+        rules: {
+            'n/exports-style': ['error', 'module.exports'],
+        },
     },
-},
+    {
+        plugins: {
+            prettier: prettierPlugin,
+        },
+    },
     {
         ignores: ['node_modules', 'dist'],
     },
